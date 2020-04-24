197145297030086f6cfef3e5846e7e7020d609f2
Sandra Vilder <sandra.vilder@gmail.com>
2020-04-24 16:08:16 +0200

    Des photos de chèvres gaa Trop bien

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: 'Client-ID uiDe48jocLZPZHLb7JXKoHzfRNB2CAVDMGOc3_jcGNc'
-      }
-    });
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+        params: { query: term }
+      });
+    this.setState({images: response.data.results});
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   };
