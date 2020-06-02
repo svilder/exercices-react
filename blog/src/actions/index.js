2ebced680066cd5f0ab6143464569a9aecec264c
Sandra Vilder <sandra.vilder@gmail.com>
2020-06-02 18:17:27 +0200

    blog ex

---
+++ blog/src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceHolder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response })
+};
