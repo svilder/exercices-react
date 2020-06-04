e92f77c37f1706c6a7452d9272eaaf2b96f664da
Sandra Vilder <sandra.vilder@gmail.com>
2020-06-04 16:33:00 +0200

    its a wrap

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -5,8 +5,15 @@ import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach(id => dispatch(fetchUser(id)));
+//  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+//  userIds.forEach(id => dispatch(fetchUser(id)));
+// refactor : 
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
