fac4bd649550e032b5ea4089aa484150b0ea2c4f
Sandra Vilder <sandra.vilder@gmail.com>
2020-07-20 18:51:07 +0200

    context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends React.Component {
+  static contextType = LanguageContext;
+
+  render() {
+    const text = this.context === 'english' ? 'submit' : 'voorlaggen';
+
+    return <button className="ui button primary">{text}</button>;
+  }
+}
+
+export default Button;
