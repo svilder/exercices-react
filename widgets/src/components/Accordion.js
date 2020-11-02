31c28f929bf8fa9dad9dc6a4b63ddb680d6024f3
Sandra Vilder <sandra.vilder@gmail.com>
2020-11-02 14:27:19 +0100

    accordion active class set up

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -9,12 +9,13 @@ const Accordion = ({ items }) => {
   }
 
   const renderedItems = items.map((item, index) => {
+    const active = index === acitveIndex ? 'active' : '';
     return <React.Fragment key={item.title}>
-      <div className="title active" onClick={() => onTitleClick(index)}>
+      <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
         <i className="dropdown icon"></i>
         {item.title}
       </div>
-      <div className="content active">
+      <div className={`content ${active}`}>
         <p>{item.content}</p>
       </div>
     </React.Fragment>
@@ -22,7 +23,6 @@ const Accordion = ({ items }) => {
   return (
     <div className="ui styled accordion">
       {renderedItems}
-      <h1>{acitveIndex}</h1>
     </div>
   );
 };
