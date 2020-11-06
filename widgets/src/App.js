611ce84af049e29539201fde95fa8d95c064bd8a
Sandra Vilder <sandra.vilder@gmail.com>
2020-11-06 13:47:27 +0100

    state in search

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Accordion from './components/Accordion';
+//import Accordion from './components/Accordion';
 import Search from './components/Search';
 
 const items = [
@@ -21,7 +21,7 @@ const items = [
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
       <Search />
     </div>
   );
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Search = () => {
+  const [ term, setTerm ] = useState('');
+
   return (
-    <div>search</div>
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input value={term} onChange={e => setTerm(e.target.value)} className="input"></input>
+        </div>
+      </div>
+    </div>
   );
 }
 
